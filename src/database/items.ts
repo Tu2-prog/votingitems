144202cbd2bfa8888ecf2235d818a2be3c3ddf7b
Tu2-prog <anhtuduong.nguyen@outlook.de>
2024-01-02 19:58:53 +0100

    removed time from model and set this as a unified proposal

--- src/database/items.ts
+++ src/database/items.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const ItemSchema = new mongoose.Schema({
     title: {type: String, required: true},
     location: {type: String, required: true},
-    time: {type: Date}
 })
 
 export const ItemModel = mongoose.model('VotingApp', ItemSchema, "VotingItems");
