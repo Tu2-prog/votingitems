772985138575e716a0aa3300d5c170d8de927e4d
Tu2-prog <anhtuduong.nguyen@outlook.de>
2024-01-02 20:16:10 +0100

    updated ItemSchema with no. of votes

--- src/database/items.ts
+++ src/database/items.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const ItemSchema = new mongoose.Schema({
     title: {type: String, required: true},
     location: {type: String, required: true},
+    no_votes: {type: Number}
 })
 
 export const ItemModel = mongoose.model('VotingApp', ItemSchema, "VotingItems");
