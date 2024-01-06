256903e9b813884c4e4f85c4b1e76f7ec227dad9
Tu2-prog <anhtuduong.nguyen@outlook.de>
2024-01-06 17:03:38 +0100

    add documentation dependency and add docstrings to the core functions

--- src/adapter/controller.ts
+++ src/adapter/controller.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import { getVotingItems, createVotingItem, getVotingItemById, deleteItemById } from "../database/items";
 
+/**
+ * Function to get all the documents in the NoSQL database.
+ * @param res The response to the request made to the specific route of this microservice.
+ * @returns Status Code of the http request.
+ */
 export const getItems = async(req: express.Request, res: express.Response) => {
     try{
         const items = await getVotingItems();
@@ -12,6 +17,12 @@ export const getItems = async(req: express.Request, res: express.Response) => {
     }
 }
 
+/**
+ * Function to create a new document into the NoSQL database.
+ * @param req The request to this microservice route with the parameters that are saved into the database.
+ * @param res The response to this request after the operation was either successful or not.
+ * @returns Status code of the http request.
+ */
 export const create = async(req: express.Request, res: express.Response) => {
     try{
         const {title, location} = req.body;
@@ -31,6 +42,12 @@ export const create = async(req: express.Request, res: express.Response) => {
     }
 }
 
+/**
+ * Function to increment the number of votes in a specific document.
+ * @param req The request containing the id of the target document.
+ * @param res The response to this request after the operation was either successful or not.
+ * @returns Status code of the http request.
+ */
 export const updateItem = async (req: express.Request, res: express.Response) => {
     try {
       const { id } = req.params;
@@ -47,6 +64,12 @@ export const updateItem = async (req: express.Request, res: express.Response) =>
     }
 }
 
+/**
+ * Function to delete a specific document from the NoSQL database.
+ * @param req The request containing the id of the target document.
+ * @param res The response to this request after the operation was either successful or not.
+ * @returns Status code of the http request.
+ */
 export const deleteItem = async (req: express.Request, res: express.Response) => {
     try {
       const { id } = req.params;
